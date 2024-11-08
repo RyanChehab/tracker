4fee8498fec5f0990c23703ac32bff29045e4b6e
unknown <ryanchehab20@gmail.com>
2024-11-09 01:55:56 +0200

    injected the user into the database

--- backend/signup.php
+++ backend/signup.php
@@ -2,9 +2,22 @@
 include 'connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
+    $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
 
     $hashed_pass = password_hash($password, PASSWORD_DEFAULT);
 
+    $sql = "INSERT INTO users(username,email,password) VALUES (?,?,?)";
+    
+    $stmt = $connection->prepare($sql);
+
+    $stmt->bind_param("sss", $username,$email,$hashed_pass);
+
+    if($stmt->execute()){
+        header("Location:../index.html");
+        
+    }else {
+        echo "error: " . $stmt->error;
+    }
 }

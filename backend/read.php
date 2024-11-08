ea17e33d6cbfee85847132b44ba58548ba4886ab
unknown <ryanchehab20@gmail.com>
2024-11-08 12:02:59 +0200

    added the sql stmt

--- backend/read.php
+++ backend/read.php
@@ -1,1 +1,12 @@
 <?php
+include 'connection.php';
+
+if ($_SERVER["REQUEST_METHOD"]=="POST"){
+    $sql="SELECT * FROM transactions";
+    
+    $query = $connection->prepare($sql);
+    
+    $query->excute();
+
+    $result = $query->get_result();
+}

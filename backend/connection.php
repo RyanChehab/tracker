cfb8b4dd9cf8e68ac25cd69c347f2cd626367ca2
unknown <ryanchehab20@gmail.com>
2024-11-08 03:05:18 +0200

    connected to the database

---
+++ backend/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = 'localhost';
+$username = 'root';
+$pass = '';
+$db = 'tracker';
+
+$connection = new mysqli($host,$username,$pass,$db);
+
+if ($connection->connect_error){
+    die("error conncting to db");
+}
+else{
+    echo("connected succesfully");
+}

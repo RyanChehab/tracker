c581b6e84153167941c41d919d75352ec7030b6b
unknown <ryanchehab20@gmail.com>
2024-11-09 05:19:45 +0200

    changed the sql to include the user_id

--- backend/read.php
+++ backend/read.php
@@ -1,12 +1,20 @@
 <?php
+session_start();
 include 'connection.php';
+$user_id=$_SESSION['user_id'];
 
 if ($_SERVER["REQUEST_METHOD"]=="POST"){
-    $sql="SELECT * FROM transactions";
+    $sql="SELECT * FROM transactions where user_id = ?";
     
-    $query = $connection->prepare($sql);
-    
-    $query->excute();
+    $stmt = $connection->prepare($sql);
 
-    $result = $query->get_result();
+    $stmt->bind_param('i',$user_id);
+
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    
+} else{
+    $response = ["message" => "Empty result"];
+    echo json_encode($response);
 }

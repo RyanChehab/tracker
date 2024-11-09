837543d1cd1b492ab7e89ffeab6efcadf41e64ca
unknown <ryanchehab20@gmail.com>
2024-11-09 04:50:36 +0200

    added user_id from session

--- backend/create.php
+++ backend/create.php
@@ -1,20 +1,27 @@
 <?php
+session_start();
 include 'connection.php';
 
 if($_SERVER["REQUEST_METHOD"]== "POST") {
-    $user_id = $_POST['user_id'];
-    $type = $_POST['type'];
-    $amount = $_POST['amount'];
-    $date = $_POST['date'];
-    $notes = $_POST['notes'];
 
-$sql = "INSERT INTO transactions (user_id, type, amount, date, notes) VALUES (?, ?, ?, ?, ?)";
-$stmt = $connection->prepar($sql);
+    $user_id = $_SESSION['user_id'];
+
+    $data = json_decode(file_get_contents("php://input"));
+    if($data){ 
+        $type = $data->type;
+        $amount = $data->amount;
+        $date = $data->date;
+        $notes = $data->description;
+    }
+   
+$sql = "INSERT INTO transactions (user_id,type, amount, date, notes) VALUES (? ,?, ?, ?, ?)";
+$stmt = $connection->prepare($sql);
 $stmt->bind_param("isdss",$user_id,$type,$amount,$date,$notes);
 
 if ($stmt->execute()) {
-    echo "Transaction added successfully!";
-} else {
-    echo "Error: " . $stmt->error;
+    echo json_encode(["message" => "Transaction added successfully!"]);
+} 
+else {
+    echo json_encode("Error:") ;
 }
 }

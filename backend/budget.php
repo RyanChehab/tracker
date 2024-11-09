9285a8d473029c9853eba588bc467cb7f152df3b
unknown <ryanchehab20@gmail.com>
2024-11-09 06:23:59 +0200

    added sql query to retrieve the sum of income

--- backend/budget.php
+++ backend/budget.php
@@ -1,3 +1,20 @@
 <?php
 session_start();
 include "connection.php";
+
+if($_SESSION['REQUEST_METHOD']=="POST"){
+    
+    $incomequery="SELECT sum(amount) as total_income FROM transactions WHERE user_id=? AND type = 'income'";
+
+    $incomeStmt = $connection->prepare($incomeQuery);
+
+    $incomeStmt->bind_param("i", $user_id);
+ 
+    $incomeStmt->execute();
+ 
+    $incomeResult = $incomeStmt->get_result();
+ 
+    $incomeRow = $incomeResult->fetch_assoc();
+ 
+    $total_income = $incomeRow['total_income'] ?? 0;
+}
